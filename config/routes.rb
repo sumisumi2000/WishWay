2ffc1ddaf4871971454c54f98f151ecfb1329aa9
川嶋澄生 <sumio.vantan308@gmail.com>
2024-03-06 17:12:14 +0900

    add new file of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  root 'staticpages#top'
+  root 'users#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
