8363f0fc5ddc4b3d3486dd716c2b35c7d3620a01
川嶋澄生 <sumio.vantan308@gmail.com>
2024-03-07 20:44:09 +0900

    add new files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    fontFamily: {
+      kiwimaru: ['Kiwi Maru']
+    }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["retro"],
+  },
+}
