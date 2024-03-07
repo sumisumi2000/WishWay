574d3760143cfe2325a930243ee70867657108d4
川嶋澄生 <sumio.vantan308@gmail.com>
2024-03-07 20:38:40 +0900

    feat: introduce TailwindCSS & DaisyUI & Google Fonts

--- Gemfile
+++ Gemfile
@@ -69,3 +69,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem 'cssbundling-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-  root 'users#index'
+  root 'staticpages#top'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
