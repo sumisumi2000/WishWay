766f65981efc252638f18964c38f9477cfdabb77
川嶋澄生 <sumio.vantan308@gmail.com>
2024-03-07 22:19:45 +0900

    reset render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate
