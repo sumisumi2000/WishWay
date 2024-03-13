ff7f14aeb561e8fd475dbcb67969cbda312367cc
川嶋澄生 <sumio.vantan308@gmail.com>
2024-03-13 14:59:25 +0900

    update: access control

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -17,7 +17,28 @@ class WishesController < ApplicationController
   end
 
   def edit
+    begin
+      # 編集する Wish をログインユーザーから取得
+      @wish = current_user.wish_list.wishes.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      # 他のユーザーの Wish 編集ページにアクセスした際に、一覧ページへリダイレクトする
+      redirect_to wish_lists_path, notice: '編集権限がありません'
+      return
+    end
+  end
+
+  def update
+    # 更新する Wish をログインユーザーから取得
     @wish = current_user.wish_list.wishes.find(params[:id])
+
+    if @wish.update(wish_params)
+      redirect_to(wish_list_path(@wish.wish_list.user), notice: 'Wish を更新しました')
+    else
+      # 変更前の値に戻す
+      @wish.update(@wish.attributes_in_database)
+      flash.now[:alert] = 'Wish の更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
