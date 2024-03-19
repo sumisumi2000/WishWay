398ae89a3309ffa6a65f84a7f3330bcfd20812dd
川嶋澄生 <sumio.vantan308@gmail.com>
2024-03-19 22:21:27 +0900

    update: change hash rocket to to:

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,23 @@ Rails.application.routes.draw do
   # ユーザー
   resources :users, only: %i[new create]
   # ログイン
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => "user_sessions#create"
-  delete 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login', to: 'user_sessions#new', as: :login
+  post 'login', to: "user_sessions#create"
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
   # WishList
   resources :wish_lists, only: %i[show index]
-  post 'lock' => 'wish_lists#lock'
-  post 'unlock' => 'wish_lists#unlock'
+  post 'lock', to: 'wish_lists#lock'
+  post 'unlock', to: 'wish_lists#unlock'
   # Wish
   resources :wishes, only: %i[new create edit update destroy]
-  post 'check/:id' => 'wishes#check', :as => :check
-  post 'uncheck/:id' => 'wishes#uncheck', :as => :uncheck
+  post 'check/:id', to: 'wishes#check', as: :check
+  post 'uncheck/:id', to: 'wishes#uncheck', as: :uncheck
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up", to: "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
