653c9805c1ceea2633c40742c9712e92e95a8687
川嶋澄生 <sumio.vantan308@gmail.com>
2024-05-27 17:11:38 +0900

    リセットパスワードのサブモジュールを作成

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -4,7 +4,7 @@
 # Available submodules are: :user_activation, :http_basic_auth, :remember_me,
 # :reset_password, :session_timeout, :brute_force_protection, :activity_logging,
 # :magic_login, :external
-Rails.application.config.sorcery.submodules = []
+Rails.application.config.sorcery.submodules = [:reset_password]
 
 # Here you can configure each submodule's features.
 Rails.application.config.sorcery.configure do |config|
