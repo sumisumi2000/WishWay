ac93c8fa033ea49fffd3013d112ac0389cdce412
川嶋澄生 <sumio.vantan308@gmail.com>
2024-06-23 11:10:43 +0900

    credentials を使用するように変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
+    user_name: Rails.application.credentials.gmail.gmail_adress,
+    password: Rails.application.credentials.gmail.gmail_password
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
