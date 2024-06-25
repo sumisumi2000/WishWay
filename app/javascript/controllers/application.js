d4649ad44c63e1b358de3115e5fdff13b09a0896
川嶋澄生 <sumio.vantan308@gmail.com>
2024-06-25 14:52:25 +0900

    stimulus-autocomplete の設定を追加

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
+// コンポーネントを読み込むための記述
+import { Autocomplete } from "stimulus-autocomplete";
 
-const application = Application.start()
+const application = Application.start();
+// コンポーネントにあるAutocompleteコントローラを使用するための記述
+application.register("autocomplete", Autocomplete);
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
