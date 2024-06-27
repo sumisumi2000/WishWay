8118557319bb6c2f42fe752c7496116d9245b056
川嶋澄生 <sumio.vantan308@gmail.com>
2024-06-27 22:47:59 +0900

    お気に入りページからお気に入りを削除した時に非同期でページからリストを削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,6 @@
 class FavoritesController < ApplicationController
+  helper_method :favorites_page?
+
   def index
     @q = current_user.favorite_wish_lists.ransack(params[:q])
     @wish_lists = @q.result(distinct: true).includes(:user).where(is_public: true).order('created_at DESC').page(params[:page])
@@ -16,10 +18,18 @@ class FavoritesController < ApplicationController
     flash.now[notice] = 'リストをお気に入りから削除しました'
   end
 
-    # オートコンプリート
-    def search
-      # 入力単語がタイトルに含まれるリストを取得
-      @wish_lists = current_user.favorite_wish_lists.where("title like ?", "%#{params[:q]}%").limit(15).order(title: :desc)
-      render partial: 'shared/search_result'
-    end
+  # オートコンプリート
+  def search
+    # 入力単語がタイトルに含まれるリストを取得
+    @wish_lists = current_user.favorite_wish_lists.where("title like ?", "%#{params[:q]}%").limit(15).order(title: :desc)
+    render partial: 'shared/search_result'
+  end
+
+  private
+
+  # 遷移元がお気に入り一覧ページかどうかを判定
+  def favorites_page?
+    request.referer == favorites_url
+  end
+
 end
