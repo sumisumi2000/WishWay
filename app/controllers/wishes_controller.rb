c34ceca87638ef14d76915b3d7b455f98a6678d9
川嶋澄生 <sumio.vantan308@gmail.com>
2024-06-28 21:27:53 +0900

    グラフの更新を非同期で処理

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,11 +1,13 @@
 class WishesController < ApplicationController
   def create
+    # ログインユーザーの WishList を取得
+    @wish_list = current_user.wish_list
     # ログインユーザーの WishList に Wish を作成
-    @wish = current_user.wish_list.wishes.build(wish_params)
+    @wish = @wish_list.wishes.build(wish_params)
     # データベースに保存
     @wish.save!
     # 達成状況を更新
-    current_user.wish_list.update_granted_wish_rate
+    @wish_list.update_granted_wish_rate
   end
 
   def edit
@@ -33,45 +35,53 @@ class WishesController < ApplicationController
   end
 
   def destroy
+    # ログインユーザーの WishList を取得
+    @wish_list = current_user.wish_list
     # ログインユーザーの Wish を全て取得
-    @wishes = current_user.wish_list.wishes
+    @wishes = @wish_list.wishes
     # 削除する Wish をログインユーザーから取得
     @wish = @wishes.find(params[:id])
     # 削除
     @wish.destroy!
     # 達成状況を更新
-    current_user.wish_list.update_granted_wish_rate
+    @wish_list.update_granted_wish_rate
   end
 
   def check
+    # ログインユーザーの WishList を取得
+    @wish_list = current_user.wish_list
     # チェックされたWish をログインユーザーから取得
-    @wish = current_user.wish_list.wishes.find(params[:id])
+    @wish = @wish_list.wishes.find(params[:id])
     # チェックする
     @wish.granted = true
     # データベースに保存
     @wish.save!
     # 達成状況を更新
-    current_user.wish_list.update_granted_wish_rate
+    @wish_list.update_granted_wish_rate
   end
 
   def uncheck
+    # ログインユーザーの WishList を取得
+    @wish_list = current_user.wish_list
     # チェックされたWish をログインユーザーから取得
-    @wish = current_user.wish_list.wishes.find(params[:id])
+    @wish = @wish_list.wishes.find(params[:id])
     # チェックを外す
     @wish.granted = false
     # データベースに保存
     @wish.save!
     # 達成状況を更新
-    current_user.wish_list.update_granted_wish_rate
+    @wish_list.update_granted_wish_rate
   end
 
   def add
+    # ログインユーザーの WishList を取得
+    @wish_list = current_user.wish_list
     # params から wish を取得
     @wish = Wish.find(params[:id])
     # ログインユーザーの wish として新規作成
-    current_user.wish_list.wishes.create!(title: @wish.title)
+    @wish_list.wishes.create!(title: @wish.title)
     # 達成状況を更新
-    current_user.wish_list.update_granted_wish_rate
+    @wish_list.update_granted_wish_rate
     # フラッシュメッセージを登録
     flash.now[:notice] = "マイリストに Wish を作成しました"
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def turbo_stream_flash
     turbo_stream.update "flash", partial: "shared/flash"
   end
+
+  def turbo_stream_graph
+    turbo_stream.update "graph", partial: "wish_lists/graph", locals: { wish_list: current_user.wish_list }
+  end
 end
