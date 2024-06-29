cf0c752d7c1e457c258564121a60ef3ddc83733d
川嶋澄生 <sumio.vantan308@gmail.com>
2024-06-29 22:05:34 +0900

    利用規約ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
   # 問い合わせフォーム
   get '/form', to: 'staticpages#form', as: :inquiry_form
   get '/policy', to: 'staticpages#policy', as: :privacy_policy
+  get '/term', to: 'staticpages#term', as: :term
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
