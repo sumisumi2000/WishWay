da2c9848624fd188a05a33c903917b584bc18dcc
sumisumi2000 <sumio.vantan308@gmail.com>
2024-07-27 15:56:42 +0900

    テスト失敗時にメッセージを表示しないように変更

--- spec/models/wish_list_spec.rb
+++ spec/models/wish_list_spec.rb
@@ -12,23 +12,23 @@ RSpec.describe WishList, type: :model do
     context '失敗パターン' do
       it 'title が空の場合、バリデーションが機能して invalid になるか' do
         wish_list = build(:wish_list, title: '')
-        expect(wish_list).to be_invalid, 'title が空です'
+        expect(wish_list).to be_invalid
       end
       it 'title が256文字以上の場合、バリデーションが機能して invalid になるか' do
         wish_list = build(:wish_list, title: 'a' * 256)
-        expect(wish_list).to be_invalid, '文字数が256文字以上です'
+        expect(wish_list).to be_invalid
       end
       it 'is_public が空の場合、バリデーションが機能して invalid になるか' do
         wish_list = build(:wish_list, is_public: nil)
-        expect(wish_list).to be_invalid, 'is_public が空です'
+        expect(wish_list).to be_invalid
       end
       it 'granted_wish_rate が 0 未満の場合、バリデーションが機能して invalid になるか' do
         wish_list = build(:wish_list, granted_wish_rate: -20)
-        expect(wish_list).to be_invalid, 'granted_wish_rate が0未満です'
+        expect(wish_list).to be_invalid
       end
       it 'granted_wish_rate が 101以上の場合、バリデーションが機能して invalid になるか' do
         wish_list = build(:wish_list, granted_wish_rate: 200)
-        expect(wish_list).to be_invalid, 'granted_wish_rate が101以上です'
+        expect(wish_list).to be_invalid
       end
     end
   end
