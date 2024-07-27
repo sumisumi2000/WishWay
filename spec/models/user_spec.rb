b548f65ee49f744d7c378e8b02a9a6a23b9bcf10
sumisumi2000 <sumio.vantan308@gmail.com>
2024-07-27 15:54:24 +0900

    テスト失敗時のメッセージを表示しないように変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,36 +18,36 @@ RSpec.describe User, type: :model do
     context '失敗パターン' do
       it 'password が3文字未満の場合、バリデーションが機能して invalid になるか' do
         user = build(:user, password: 'xx')
-        expect(user).to be_invalid, 'password が3文字未満です'
+        expect(user).to be_invalid
       end
       it 'password_confirmation が空の場合、バリデーションが機能して invalid になるか' do
         user = build(:user, password_confirmation: '')
-        expect(user).to be_invalid, 'password_confirmation が空です'
+        expect(user).to be_invalid
       end
       it 'password と password_confirmation が一致しない場合、バリデーションが機能して invalid になるか' do
         user = build(:user, password: 'password', password_confirmation: 'password_fake')
-        expect(user).to be_invalid, 'passord と password_confirmation が一致していないです'
+        expect(user).to be_invalid
       end
       it 'email が一意の値でない（既存のデータと重複する）場合、バリデーションが機能して invalid になるか' do
         user = create(:user)
         duplicated_user = build(:user, email: user.email)
-        expect(duplicated_user).to be_invalid, 'email の値が既存のデータと重複しています'
+        expect(duplicated_user).to be_invalid
       end
       it 'email が空の場合、バリデーションが機能して invalid になるか' do
         user = build(:user, email: '')
-        expect(user).to be_invalid, 'email が空です'
+        expect(user).to be_invalid
       end
       it 'email が256文字以上の場合、バリデーションが機能して invalid になるか' do
         user = build(:user, email: 'a' * 256)
-        expect(user).to be_invalid, 'email が256文字以上です'
+        expect(user).to be_invalid
       end
       it 'name が空の場合、バリデーションが機能して invalid になるか' do
         user = build(:user, name: '')
-        expect(user).to be_invalid, 'name が空です'
+        expect(user).to be_invalid
       end
       it 'name が51文字以上の場合、バリデーションが機能して invalid になるか' do
         user = build(:user, name: 'a' * 51)
-        expect(user).to be_invalid, 'name が51文字以上です'
+        expect(user).to be_invalid
       end
     end
   end
